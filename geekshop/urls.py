0f3725bdb706689ceb9021c764b0c4aefb8938ac
kondra <kondraa@gmail.com>
2022-01-11 10:00:29 +0200

    added views,urls, forms.py .

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls', namespace='mainapp')),
+    path('users/', include('users.urls', namespace='users')),
 
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import UserLoginForm
+
+
+def login(request):
+    form = UserLoginForm()
+    context = {
+        'title': 'Geekshop Authorization',
+        'form': form,
+
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'Geekshop Registration'
+    }
+    return render(request, 'users/register.html', context)
