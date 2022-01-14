e84a10180b44c14bc57d584eb3d52d1949bac4fd
kondra <kondraa@gmail.com>
2022-01-14 10:04:31 +0200

    custom userLogin and UserRegister forms

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from .models import User
 
@@ -7,4 +7,29 @@ class UserLoginForm(AuthenticationForm):
 
     class Meta():
         model = User
-        fields = ('login', 'password')
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Fill out username'
+        self.fields['password'].widget.attrs['placeholder'] = 'Fill out password'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class UserRegisterForm(UserCreationForm):
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1','password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Fill out firstname'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Fill out lastname'
+        self.fields['email'].widget.attrs['placeholder'] = 'Fill out email'
+        self.fields['username'].widget.attrs['placeholder'] = 'Fill out username'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Fill out password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Fill out password one more time '
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
